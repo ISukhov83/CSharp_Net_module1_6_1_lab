11cb4e9a16bab7dc5a705b59aab5f38d86dc3417
ISukhov83 <isukhov83@gmail.com>
2016-03-26 15:57:05 +0200

    Add_Multithreading_Lab

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp_Net_module1_6_1_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadManipulator My_TherdManipulator = new ThreadManipulator();
+            Thread AddOne_1 = new Thread(new ParameterizedThreadStart(My_TherdManipulator.AddingOne));
+            Thread AddOne_2 = new Thread(new ParameterizedThreadStart(My_TherdManipulator.AddingOne));
+            Thread AddCustomValue_3 = new Thread(new ParameterizedThreadStart(My_TherdManipulator.AddingCustomValue));
+            Thread Stop_4 = new Thread(new ThreadStart(My_TherdManipulator.Stop));
+            Stop_4.IsBackground = true;
+            AddOne_1.Start(10);
+            AddOne_2.Start(20);
+            AddCustomValue_3.Start(new Int32[2] {5, 15});
+
+            AddOne_1.Join();
+            AddOne_2.Join();
+            AddCustomValue_3.Join();
+            Stop_4.Start();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ ThreadManipulator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp_Net_module1_6_1_lab
+{
+
+
+    class ThreadManipulator
+    {
+        private ConsoleKey Pressed_ConsoleKey;
+        private Int32 Input_Value_Int = 10;
+        private Int32[] Int_Array;
+        public void AddingOne(object Input_Value)
+        {
+            lock(this)
+            {
+                Input_Value_Int = (Int32)Input_Value;
+                for (Int32 i = 1; i <= 20; i++)
+                {
+                    if (Pressed_ConsoleKey == ConsoleKey.Q)
+                        break;
+                    Console.WriteLine("AddOne. Start_Value={0}, Iteration_Number={1}, Current_Value={2}", Input_Value_Int, i, Input_Value_Int * i);
+                    Thread.Sleep(500);
+                }
+                
+            }
+        }
+        public void AddingCustomValue(object Input_Value)
+        {
+            for(Int32 i=0;i<=19;i++)
+            {
+                if (Pressed_ConsoleKey == ConsoleKey.W)
+                    break;
+                Int_Array = (Int32[])Input_Value;
+                Console.WriteLine("AddCustomValue. Start_Value={0}, Step_Value={1}, Iteration_Number={2}, Current_Value={3}",
+                                   Int_Array[0], Int_Array[1], i +1, Int_Array[0] + (Int_Array[1] * i));
+                Thread.Sleep(500);
+            }
+        }
+        public void Stop()   /*не получилось отслеживать нажатые клавиши и передавать в переменную Pressed_ConsoleKey для остаонвки методов*/
+        {
+            Pressed_ConsoleKey = Console.ReadKey().Key;
+        }
+
+    }
+}
